097cc85a497c9a4967b8712fc24e25a33f9a64c4
khj <guswn@naver.com>
2020-02-07 14:50:07 +0900

    스프링 인터셉터 구현한 인증 체크 완료 수정

--- src/main/java/com/cos/blog/controller/PostController.java
+++ src/main/java/com/cos/blog/controller/PostController.java
@@ -6,43 +6,43 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.cos.blog.model.user.User;
 
 @Controller
 public class PostController {
-	
+
 	@Autowired
 	private HttpSession session;
-	
-	@GetMapping({"", "/", "/post"})
+
+	@GetMapping({ "", "/", "/post" })
 	public String posts() {
 		return "/post/list";
 	}
-	
+
 	@GetMapping("/post/{id}")
 	public String post() {
 		return "/post/detail";
 	}
-	
+
 	// 인증 체크
 	@GetMapping("/post/write")
 	public String write() {
-		
+
 		return "/post/write";
-		
+
 	}
+
 	// 인증 체크 + 동일인 체크
-	@GetMapping("/post/update/{id}")
-	public String update(@PathVariable int id) {
-		
-		User principal = (User)session.getAttribute("principal"); 
-		if(principal.getId()==id) {
-			return "/post/update";
-		}else {
+	@GetMapping("/post/update/{postId}")
+	public String update(@PathVariable int id, @RequestParam int userId) {
+
+		User principal = (User) session.getAttribute("principal");
+		if (principal.getId() != userId) {
 			return "/post/login";
 		}
-		
+		return "/post/update";
 	}
-	
+
 }
